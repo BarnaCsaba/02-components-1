47373538acdbd0929e502825cff18ed25895eb8c
BarnaCsaba_PG <barna.csaba@diak.szbi-pg.hu>
2026-09-25 10:15:12 +0200

    x

---
+++ 03-props-inside-script/src/components/SiteContent.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <main><p>This is the main content of the page.</p></main>
+</template>
+
+<style scoped>
+main {
+    padding: 20px;
+    background-color: aqua;
+}</style>
---
+++ 03-props-inside-script/src/components/SiteFooter.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <p>&copy; 2026 My website. All rights reserved.</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: blueviolet;
+}
+</style>
