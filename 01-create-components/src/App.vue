472d7ce00f5ccab40d685fb4571f16062710f901
BarnaCsaba_PG <barna.csaba@diak.szbi-pg.hu>
2026-09-25 08:53:15 +0200

    sitefooter

--- 01-create-components/src/App.vue
+++ 01-create-components/src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
     import MainNavigation from './components/MainNavigation.vue';
+    import SiteFooter from './components/SiteFooter.vue';
 </script>
 
 <template>
     <header>
         <MainNavigation />
     </header>
+    <footer>
+        <!-- sitefooter komponens használata -->
+        <SiteFooter/>
+    </footer>
 </template>
 
 <style scoped>
---
+++ 01-create-components/src/components/SiteFooter.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <p>&copy; 2026 My website. All rights reserved.</p>
+  </footer>
+</template>
+
+<style scoped></style>
